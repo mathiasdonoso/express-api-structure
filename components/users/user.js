d2693ca140251b396cc37814c61225568ae8d9d2
Mathías <mathiasd88@gmail.com>
2018-10-27 13:03:12 -0300

    Simplificación de respuesta inicial

--- components/users/user.js
+++ components/users/user.js
@@ -1,11 +1,1 @@
-class User {
-  constructor(name) {
-    this.name = name;
-  }
-
-  greet() {
-    return `Hello, my name is ${this.name}`;
-  }
-}
-
-module.exports = User;
+module.exports = {};
--- components/users/users.controller.js
+++ components/users/users.controller.js
@@ -1,9 +1,6 @@
-const User = require('./user');
-
 const hello = (req, res) => {
-  const user = new User('Mathías');
   res.status(200).json({
-    message: user.greet(),
+    message: 'Hello world',
   });
 };
 
